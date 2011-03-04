582153191beef913684fed79683e7a04c3295086
Diogo Resende <dresende@thinkdigital.pt>
2011-03-04 17:33:54 +0000

    added some comments to example

--- example.js
+++ example.js
@@ -12,10 +12,14 @@ orm.connect("mysql://orm:orm@localhost/orm", function (success, db) {
 			}
 		}
 	});
+	// one to one association: Person.sibling -> Person.id
 	Person.hasOne("sibling", Person);
+	// many to many association: creates table person_friends with primary keys person_id and friend_id
 	Person.hasMany("friends", Person, "friend");
+	// create table(s) on database
 	Person.sync();
 
+	// new records
 	var John = new Person({
 		"name"		: "John",
 		"surname"	: "Doe",
@@ -26,17 +30,21 @@ orm.connect("mysql://orm:orm@localhost/orm", function (success, db) {
 		"surname"	: "Doe",
 		"age"		: 18
 	});
+	
+	// show John record
 	console.log(John);
 	
-	// this will auto-save Jane
+	// this will auto-save Jane (to get Jane's ID)
 	John.setSibling(Jane, function (success) {
 		if (success) {
 			John.save(function (success) {
 				if (success) {
+					// show John record again (should have sibling_id)
 					console.log(John);
 					John.unsetSibling(function (success) {
 						John.save(function (success) {
 							if (success) {
+								// show John record again (should have sibling_id = 0)
 								console.log(John);
 							}
 						});
