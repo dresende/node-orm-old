fe4744a6d5403b4f250324358e4b5d9cd97fb301
Diogo Resende <dresende@thinkdigital.pt>
2011-10-06 15:22:16 +0100

    - add predefined validators initial code (rangeNumber, insideList, outsideList); - add predefined-validation.js example

---
+++ examples/predefined-validation.js
@@ -0,0 +1,44 @@
+var orm = require(__dirname + "/../lib/orm");
+
+orm.connect("mysql://orm:orm@localhost/orm", function (success, db) {
+	// define a Person
+	var Person = db.define("person", {
+		"created"	: { "type": "date" },
+		"name"		: { "type": "string" },
+		"surname"	: { "type": "string", "def": "" },
+		"age"		: { "type": "int" },
+		"male"		: { "type": "bool", "def": true },
+		"meta"		: { "type": "struct" }
+	}, {
+		"methods"	: {
+			// this is a method that can be called in any
+			// instance
+			"fullName" :function () {
+				return this.name + " " + this.surname;
+			}
+		},
+		"validations": {
+			"age": orm.validators.rangeNumber(18, 150)
+		}
+	});
+	Person.find({ name: "Jane" }, function (Janes) {
+		if (Janes === null) {
+			return console.log("No Jane does not exist!");
+		}
+
+		var Jane = Janes[0];
+
+		Jane.age = -15;
+		console.log("saving Jane as -15 year old..");
+		Jane.save(function (err, SavedJane) {
+			console.log("error:", err);
+			
+			Jane.age = 19;
+			console.log("saving Jane as 19 year old..");
+			Jane.save(function (err, SavedJane) {
+				console.log("error:", err);
+				console.log("Jane:", SavedJane);
+			});
+		});
+	});
+});
