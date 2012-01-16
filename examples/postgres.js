a4c7b4b50b93b107f944054293d070a25d5ce518
Diogo Resende <dresende@thinkdigital.pt>
2012-01-16 20:04:56 +0000

    Change postgres example to a more simple one. Fix postgres escaping by using native module escapes just like previously done on insertRecords(). This should fix #28

--- examples/postgres.js
+++ examples/postgres.js
@@ -1,44 +1,17 @@
 var orm = require(__dirname + "/../lib/orm");
 
-orm.connect("postgresql://postgres:postgres@localhost/amber", function (success, db) {
+orm.connect("pg://postgres:postgres@localhost/amber", function (success, db) {
 	if (!success) {
 		console.log("Error %d: %s", db.number, db.message);
 		return;
 	}
-	var FTS = db.define("fts", {
-		"name"		: { "type": "string" },
-		"body"		: { "type": "text", "textsearch": true }
+	var Person = db.define("person", {
+		   name: String,
+		created: Date
 	});
-	FTS.sync();
+	Person.sync();
 
-	var fts = [
-		new FTS({ "name": "John", "body": "John is friend of Jane and Jeremy" }),
-		new FTS({ "name": "Jane", "body": "This girl is not friendly" }),
-		new FTS({ "name": "Jeremy", "body": "This guy has no one" }),
-		new FTS({ "name": "Jessica", "body": "Jessica does not have any friends" }),
-		new FTS({ "name": "Jasmin", "body": "Jasmin is too young" })
-	];
-	FTS.clear(function () {
-		for (var i = 0; i < fts.length; i++) {
-			fts[i].save(function (err) {
-				if (err) {
-					console.log("Error %s: %s", err.number, err.message);
-					return;
-				}
-				console.dir("FTS saved");
-			});
-		}
+	Person.find({ name: [ "John Doe" ] }, function (people) {
+		console.log(people);
 	});
-
-	setTimeout(function () {
-		console.log("Searching for 'friends or jeremy or (jane and not jones)' (max 2 results)..");
-		FTS.textsearch(2, "friends or jeremy or (jane and not jones)", function (people) {
-			if (people === null) {
-				return console.log("Error searching");
-			}
-			for (i = 0; i < people.length; i++) {
-				console.log("%s (%d) = '%s'", people[i].name, people[i].id, people[i].body);
-			}
-		});
-	}, 1500);
 });
